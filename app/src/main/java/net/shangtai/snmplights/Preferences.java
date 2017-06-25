e7e9229b27c4ffe51842b5caedd116c3bddcbe4c
Staffan Thomen <duck@shangtai.net>
2017-06-26 00:35:41 +0300

    Cleaned up preferences, and load available auth and priv methods from SnmpHelper

--- app/src/main/java/net/shangtai/snmplights/Preferences.java
+++ app/src/main/java/net/shangtai/snmplights/Preferences.java
@@ -2,11 +2,20 @@ package net.shangtai.snmplights;
 
 import android.os.Bundle;
 import android.preference.Preference;
+import android.preference.ListPreference;
 import android.preference.PreferenceManager;
 import android.preference.PreferenceActivity;
 
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
+
+import net.shangtai.snmphelper.SnmpHelperMap;
+
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Collections;
 
 import android.util.Log;
 
@@ -15,6 +24,8 @@ public class Preferences extends PreferenceActivity implements OnSharedPreferenc
 	public final static String PREF_USERNAME="net.shangtai.snmplights.USERNAME";
 	public final static String PREF_AUTH_PASSWORD="net.shangtai.snmplights.AUTH_PASSWORD";
 	public final static String PREF_AUTH_PROTOCOL="net.shangtai.snmplights.AUTH_PROTOCOL";
+	public final static String PREF_PRIV_PASSWORD="net.shangtai.snmplights.PRIV_PASSWORD";
+	public final static String PREF_PRIV_PROTOCOL="net.shangtai.snmplights.PRIV_PROTOCOL";
 	public final static String PREF_HOST="net.shangtai.snmplights.HOST";
 	public final static String PREF_PORT="net.shangtai.snmplights.PORT";
 
@@ -25,9 +36,13 @@ public class Preferences extends PreferenceActivity implements OnSharedPreferenc
 		PREF_USERNAME,
 		PREF_AUTH_PASSWORD,
 		PREF_AUTH_PROTOCOL,
+		PREF_PRIV_PASSWORD,
+		PREF_PRIV_PROTOCOL,
 		PREF_HOST,
 		PREF_PORT
 	};
+
+	private Map<String,String> knownNames = new HashMap<String,String>();
 
 	private SharedPreferences prefs;
 
@@ -38,6 +53,10 @@ public class Preferences extends PreferenceActivity implements OnSharedPreferenc
 		prefs=PreferenceManager.getDefaultSharedPreferences(this);
 
 		addPreferencesFromResource(R.layout.preferences);
+
+		createKnownNamesMap();
+		loadAuthProtocols();
+		loadPrivProtocols();
 	}
 
 	@Override
@@ -57,26 +76,87 @@ public class Preferences extends PreferenceActivity implements OnSharedPreferenc
 
 	/* Implements OnSharedPreferenceChangeListener */
 	public void onSharedPreferenceChanged(SharedPreferences sp, String key) {
-		setPreference(key);
+		setPreferenceSummary(key);
 	}
 
 	public void setSummaries() {
 		int i;
 
 		for (i=0;i<keys.length;i++) {
-			setPreference(keys[i]);
+			setPreferenceSummary(keys[i]);
 		}
 	}
 
-	public void setPreference(String key) {
+	public void setPreferenceSummary(String key) {
 		Preference p;
 		p=findPreference(key);
+		String value=prefs.getString(key, "");
 
-		if (key.equals(PREF_AUTH_PASSWORD)) {
-			p.setSummary(PASSWORD_STARS);
+		if (key.equals(PREF_AUTH_PASSWORD) || key.equals(PREF_PRIV_PASSWORD)) {
+			if (value.isEmpty()) {
+				p.setSummary("");
+			} else {
+				p.setSummary(PASSWORD_STARS);
+			}
+		} else if (key.equals(PREF_AUTH_PROTOCOL) || key.equals(PREF_PRIV_PROTOCOL)) {
+			p.setSummary(getKnownListName(value));
 		} else {
-			p.setSummary(prefs.getString(key, ""));
+			p.setSummary(value);
 		}
+	}
+
+	private void createKnownNamesMap() {
+		knownNames.put("", getResources().getString(R.string.none));
+		knownNames.put("md5", "MD5");
+		knownNames.put("sha", "SHA");
+		knownNames.put("sha2", "SHA2");
+		knownNames.put("des", "DES");
+		knownNames.put("3des", "3DES");
+		knownNames.put("aes", "AES");
+		knownNames.put("aes128", "AES-128");
+		knownNames.put("aes192", "AES-192");
+		knownNames.put("aes256", "AES-256");
+	}
+
+	private void loadAuthProtocols() {
+		ListPreference p=(ListPreference)findPreference(PREF_AUTH_PROTOCOL);
+
+		ArrayList<String> options = new ArrayList<String>(Arrays.asList(SnmpHelperMap.getAvailableAuthMethods()));
+
+		Collections.sort(options);
+		options.add(0, "");
+
+		p.setEntries(optionsListToNamesArray(options));
+		p.setEntryValues(options.toArray(new String[options.size()]));
+	}
+
+	private void loadPrivProtocols() {
+		ListPreference p=(ListPreference)findPreference(PREF_PRIV_PROTOCOL);
+
+		ArrayList<String> options = new ArrayList<String>(Arrays.asList(SnmpHelperMap.getAvailablePrivMethods()));
+
+		Collections.sort(options);
+		options.add(0, "");
+
+		p.setEntries(optionsListToNamesArray(options));
+		p.setEntryValues(options.toArray(new String[options.size()]));
+	}
+
+	private String[] optionsListToNamesArray(ArrayList<String> options) {
+		int i;
+
+		for (i = 0;i<options.size();i++) {
+			options.set(i, getKnownListName(options.get(i)));
+		}
+
+		return options.toArray(new String[options.size()]);
+	}
+
+	private String getKnownListName(String key) {
+		if (knownNames.keySet().contains(key))
+			return knownNames.get(key);
+
+		return key;
 	}
 }
 
